62d22fecbcfbdeccc8c6f055fed8810f0fb68694
Hamza Arif <102205285+hamzaarif12345@users.noreply.github.com>
2023-08-31 20:14:54 +0530

    Add files via upload

---
+++ insertion_sort.cpp
@@ -0,0 +1,28 @@
+// insertion sort
+
+#include <iostream>
+using namespace std;
+
+void insertionSort(int arr[], int n) {
+    int i, key, j;
+    for (i = 1; i < n; i++) {
+        key = arr[i];
+        j = i - 1;
+        while (j >= 0 && arr[j] > key) {
+            arr[j + 1] = arr[j];
+            j = j - 1;
+        }
+        arr[j + 1] = key;
+    }
+}
+
+int main() {
+    int arr[] = { 64, 34, 25, 12, 22, 11, 90 };
+    int n = sizeof(arr) / sizeof(arr[0]);
+    insertionSort(arr, n);
+    cout << "Sorted array: ";
+    for (int i = 0; i < n; i++) {
+        cout << arr[i] << " ";
+    }
+    return 0;
+}
