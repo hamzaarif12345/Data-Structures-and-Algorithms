43e42a1cba8d2cbca28b2d40a9fca31ad290b11f
Hamza Arif <102205285+hamzaarif12345@users.noreply.github.com>
2024-02-13 10:27:10 +0530

    Add files via upload

---
+++ Leetcode_2108.cpp
@@ -0,0 +1,42 @@
+/*
+2108. Find First Palindromic String in the Array
+Solved
+Easy
+Topics
+Companies
+Hint
+Given an array of strings words, return the first palindromic string in the array. If there is no such string, return an empty string "".
+
+A string is palindromic if it reads the same forward and backward.
+
+ 
+
+Example 1:
+
+Input: words = ["abc","car","ada","racecar","cool"]
+Output: "ada"
+Explanation: The first string that is palindromic is "ada".
+Note that "racecar" is also palindromic, but it is not the first.
+Example 2:
+
+Input: words = ["notapalindrome","racecar"]
+Output: "racecar"
+Explanation: The first and only string that is palindromic is "racecar".
+Example 3:
+
+Input: words = ["def","ghi"]
+Output: ""
+Explanation: There are no palindromic strings, so the empty string is returned.
+*/
+
+class Solution {
+public:
+    string firstPalindrome(vector<string>& words) {
+        for(int i=0;i<words.size();i++){
+            string b=words[i];
+            reverse(b.begin(),b.end());
+            if(b==words[i]) return words[i];
+        }
+        return "";
+    }
+};
