9814b9fed2950e724664f01cfb27dac93e385f76
Hamza Arif <102205285+hamzaarif12345@users.noreply.github.com>
2024-03-22 11:45:44 +0530

    Add files via upload

---
+++ Leetcode_234.cpp
@@ -0,0 +1,59 @@
+/*
+234. Palindrome Linked List
+Solved
+Easy
+Topics
+Companies
+Given the head of a singly linked list, return true if it is a 
+palindrome
+ or false otherwise.
+
+ 
+
+Example 1:
+
+
+Input: head = [1,2,2,1]
+Output: true
+Example 2:
+
+
+Input: head = [1,2]
+Output: false
+ 
+
+Constraints:
+
+The number of nodes in the list is in the range [1, 105].
+0 <= Node.val <= 9
+ 
+
+Follow up: Could you do it in O(n) time and O(1) space?
+
+*/
+
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    bool isPalindrome(ListNode* head) {
+        ListNode* temp=head;
+        string a="";
+        while(temp!=nullptr){
+            a+=temp->val+'0';
+            temp=temp->next;
+        }
+        string b=a;
+        reverse(a.begin(),a.end());
+        cout<<a;
+        return a==b;
+    }
+};
