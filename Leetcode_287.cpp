ba9e00e379cef543b3aadd5f58f45795d39ee88a
Hamza Arif <102205285+hamzaarif12345@users.noreply.github.com>
2024-03-24 15:01:27 +0530

    Add files via upload

---
+++ Leetcode_287.cpp
@@ -0,0 +1,34 @@
+/*
+287
+Given an array of integers nums containing n + 1 integers where each integer is in the range [1, n] inclusive.
+
+There is only one repeated number in nums, return this repeated number.
+
+You must solve the problem without modifying the array nums and uses only constant extra space.
+
+ 
+
+Example 1:
+
+Input: nums = [1,3,4,2,2]
+Output: 2
+Example 2:
+
+Input: nums = [3,1,3,4,2]
+Output: 3
+Example 3:
+
+Input: nums = [3,3,3,3,3]
+Output: 3
+*/
+
+class Solution {
+public:
+    int findDuplicate(vector<int>& nums) {
+        unordered_map<int,int> m;
+        for (auto a :nums) m[a]++;
+        for(auto a : m) {
+            if(a.second >=2) return a.first;
+        }return -1;
+    }
+};
