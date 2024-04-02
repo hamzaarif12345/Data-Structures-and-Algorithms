b3dac40afca660fc15ef2707c225ab6c8af3dcb0
Hamza Arif <102205285+hamzaarif12345@users.noreply.github.com>
2024-04-02 11:52:58 +0530

    Add files via upload

---
+++ Leetcode_205.cpp
@@ -0,0 +1,40 @@
+/*
+205. Isomorphic Strings
+Solved
+Easy
+Topics
+Companies
+Given two strings s and t, determine if they are isomorphic.
+
+Two strings s and t are isomorphic if the characters in s can be replaced to get t.
+
+All occurrences of a character must be replaced with another character while preserving the order of characters. No two characters may map to the same character, but a character may map to itself.
+
+ 
+
+Example 1:
+
+Input: s = "egg", t = "add"
+Output: true
+Example 2:
+
+Input: s = "foo", t = "bar"
+Output: false
+Example 3:
+
+Input: s = "paper", t = "title"
+Output: true
+ */
+ 
+class Solution {
+public:
+    bool isIsomorphic(string s, string t) {
+        int m1[256] = {0}, m2[256] = {0}, n = s.size();
+        for (int i = 0; i < n; ++i) {
+            if (m1[s[i]] != m2[t[i]]) return false;
+            m1[s[i]] = i + 1;
+            m2[t[i]] = i + 1;
+        }
+        return true;
+    }
+};
