96bc4a40f738307c7944f60fc85086fb33c97755
Hamza Arif <102205285+hamzaarif12345@users.noreply.github.com>
2024-04-21 11:47:53 +0530

    Add files via upload

---
+++ Leetcode_1971.cpp
@@ -0,0 +1,70 @@
+/*
+1971. Find if Path Exists in Graph
+Solved
+Easy
+Topics
+Companies
+There is a bi-directional graph with n vertices, where each vertex is labeled from 0 to n - 1 (inclusive). The edges in the graph are represented as a 2D integer array edges, where each edges[i] = [ui, vi] denotes a bi-directional edge between vertex ui and vertex vi. Every vertex pair is connected by at most one edge, and no vertex has an edge to itself.
+
+You want to determine if there is a valid path that exists from vertex source to vertex destination.
+
+Given edges and the integers n, source, and destination, return true if there is a valid path from source to destination, or false otherwise.
+
+ 
+
+Example 1:
+
+
+Input: n = 3, edges = [[0,1],[1,2],[2,0]], source = 0, destination = 2
+Output: true
+Explanation: There are two paths from vertex 0 to vertex 2:
+- 0 → 1 → 2
+- 0 → 2
+Example 2:
+
+
+Input: n = 6, edges = [[0,1],[0,2],[3,5],[5,4],[4,3]], source = 0, destination = 5
+Output: false
+Explanation: There is no path from vertex 0 to vertex 5.
+*/
+
+#include <vector>
+#include <queue>
+#include <unordered_map>
+using namespace std;
+
+class Solution {
+public:
+    bool validPath(int n, vector<vector<int>>& edges, int source, int destination) {
+        queue<int> q;
+        unordered_map<int, bool> visited;
+        unordered_map<int, vector<int>> graph;
+        
+        // Constructing the adjacency list
+        for(int i = 0; i < edges.size(); i++) {
+            graph[edges[i][0]].push_back(edges[i][1]);
+            graph[edges[i][1]].push_back(edges[i][0]);
+        }
+        
+        q.push(source);
+        visited[source] = true;
+        
+        while (!q.empty()) {
+            int current = q.front();
+            q.pop();
+            
+            if (current == destination) {
+                return true;
+            }
+            
+            for (int neighbor : graph[current]) {
+                if (!visited[neighbor]) {
+                    visited[neighbor] = true;
+                    q.push(neighbor);
+                }
+            }
+        }
+        
+        return false;
+    }
+};
