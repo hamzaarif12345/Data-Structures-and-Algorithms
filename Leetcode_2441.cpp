48b0025f2aab03cc2d4548a6d35b7867df005bc4
Hamza Arif <102205285+hamzaarif12345@users.noreply.github.com>
2024-05-02 11:37:15 +0530

    Add files via upload

---
+++ Leetcode_2441.cpp
@@ -0,0 +1,41 @@
+/*
+2441. Largest Positive Integer That Exists With Its Negative
+Solved
+Easy
+Topics
+Companies
+Hint
+Given an integer array nums that does not contain any zeros, find the largest positive integer k such that -k also exists in the array.
+
+Return the positive integer k. If there is no such integer, return -1.
+
+ 
+
+Example 1:
+
+Input: nums = [-1,2,-3,3]
+Output: 3
+Explanation: 3 is the only valid k we can find in the array.
+Example 2:
+
+Input: nums = [-1,10,6,7,-7,1]
+Output: 7
+Explanation: Both 1 and 7 have their corresponding negative values in the array. 7 has a larger value.
+Example 3:
+
+Input: nums = [-10,8,6,7,-2,-3]
+Output: -1
+Explanation: There is no a single valid k, we return -1.
+*/
+class Solution {
+public:
+    int findMaxK(vector<int>& nums) {
+        sort(nums.begin(),nums.end());
+        int i=0,j=nums.size()-1;
+        while(i<j){
+            if(-nums[i]==nums[j]) return nums[j];
+            else if(abs(nums[i]) < abs(nums[j])) j--;
+            else i++;
+        }return -1;
+    }
+};
