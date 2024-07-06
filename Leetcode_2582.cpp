dd84bc900b736a23d247424bd27440a646c4cd2d
Hamza Arif <hamza81611@gmail.com>
2024-07-06 10:07:45 +0530

    Add files via upload

---
+++ Leetcode_2582.cpp
@@ -0,0 +1,36 @@
+/*
+2582. Pass the Pillow
+Solved
+Easy
+Topics
+Companies
+Hint
+There are n people standing in a line labeled from 1 to n. The first person in the line is holding a pillow initially. Every second, the person holding the pillow passes it to the next person standing in the line. Once the pillow reaches the end of the line, the direction changes, and people continue passing the pillow in the opposite direction.
+
+For example, once the pillow reaches the nth person they pass it to the n - 1th person, then to the n - 2th person and so on.
+Given the two positive integers n and time, return the index of the person holding the pillow after time seconds.
+
+ 
+
+Example 1:
+
+Input: n = 4, time = 5
+Output: 2
+Explanation: People pass the pillow in the following way: 1 -> 2 -> 3 -> 4 -> 3 -> 2.
+After five seconds, the 2nd person is holding the pillow.
+Example 2:
+
+Input: n = 3, time = 2
+Output: 3
+Explanation: People pass the pillow in the following way: 1 -> 2 -> 3.
+After two seconds, the 3rd person is holding the pillow.
+ 
+*/
+
+class Solution {
+    public int passThePillow(int n, int t) {
+        int rounds = t/(n-1); // how many rounds have we passed back and forth
+        int k = t % (n-1); // index starting from left or right at the last round
+        return rounds % 2 == 0 ? k+1 : n - k;
+    }
+}
