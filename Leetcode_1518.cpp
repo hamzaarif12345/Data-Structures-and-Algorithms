f0acd5343df8a98ba7b68e9d0ccb36c109d05863
Hamza Arif <hamza81611@gmail.com>
2024-07-07 12:25:51 +0530

    Add files via upload

---
+++ Leetcode_1518.cpp
@@ -0,0 +1,49 @@
+/*
+1518. Water Bottles
+Solved
+Easy
+Topics
+Companies
+Hint
+There are numBottles water bottles that are initially full of water. You can exchange numExchange empty water bottles from the market with one full water bottle.
+
+The operation of drinking a full water bottle turns it into an empty bottle.
+
+Given the two integers numBottles and numExchange, return the maximum number of water bottles you can drink.
+
+ 
+
+Example 1:
+
+
+Input: numBottles = 9, numExchange = 3
+Output: 13
+Explanation: You can exchange 3 empty bottles to get 1 full water bottle.
+Number of water bottles you can drink: 9 + 3 + 1 = 13.
+Example 2:
+
+
+Input: numBottles = 15, numExchange = 4
+Output: 19
+Explanation: You can exchange 4 empty bottles to get 1 full water bottle. 
+Number of water bottles you can drink: 15 + 3 + 1 = 19.
+ 
+*/
+
+class Solution {
+public:
+    int numWaterBottles(int numBottles, int numExchange) {
+        int ans=0;
+        ans+=numBottles; // 17
+        int  r=numBottles/numExchange;//5
+        int  l=numBottles%numExchange;//2
+        ans+=r;
+        while(r+l >= numExchange){
+        numBottles=r+l;
+
+        r=numBottles/numExchange;
+        l=numBottles%numExchange;
+        ans+=r;}
+        return ans;
+    }
+};
