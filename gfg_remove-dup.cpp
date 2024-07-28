d9eff2f82c724794108084915a616f635d697b33
Hamza Arif <hamza81611@gmail.com>
2024-07-28 12:49:11 +0530

    Add files via upload

---
+++ gfg_remove-dup.cpp
@@ -0,0 +1,47 @@
+//https://www.geeksforgeeks.org/problems/remove-duplicates3034/1
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+// User function template for C++
+class Solution {
+  public:
+
+    string removeDups(string str) {
+        // Your code goes here
+        unordered_map<char,int> m;
+        string ans="";
+        for(auto a:str){
+            if(m[a]==1) continue;
+            else{
+                m[a]=1;
+                ans+=a;
+            }
+        }
+        return ans;
+    }
+};
+
+//{ Driver Code Starts.
+
+int main() {
+
+    ios_base::sync_with_stdio(0);
+    cin.tie(NULL);
+    cout.tie(NULL);
+
+    int t;
+    cin >> t;
+    while (t--) {
+        string s;
+        cin >> s;
+
+        Solution ob;
+        cout << ob.removeDups(s) << "\n";
+    }
+
+    return 0;
+}
+// } Driver Code Ends
