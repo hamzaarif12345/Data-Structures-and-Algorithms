3d31ea07a60b002c8392bfa3e592ce17d3e5d76f
Hamza Arif <hamza81611@gmail.com>
2024-08-18 11:16:53 +0530

    Add files via upload

---
+++ gfg_split-array-two.cpp
@@ -0,0 +1,51 @@
+//https://www.geeksforgeeks.org/problems/split-an-array-into-two-equal-sum-subarrays/1
+
+
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+class Solution {
+  public:
+    bool canSplit(vector<int>& arr) {
+        // code here
+        int sum=0;
+        for(auto a:arr){
+            sum+=a;
+        }
+        int temp=0;
+        for(int i=0;i<arr.size();i++){
+            temp+=arr[i];
+            sum-=arr[i];
+            if(sum==temp) return true;
+        }
+        return false;
+    }
+};
+
+
+//{ Driver Code Starts.
+
+int main() {
+    int t;
+    cin >> t;
+    cin.ignore();
+    while (t-- > 0) {
+        string s;
+        getline(cin, s);
+        stringstream ss(s);
+        vector<int> arr;
+        string temp;
+        while (ss >> temp) {
+            arr.push_back(stoi(temp));
+        }
+
+        Solution obj;
+        bool res = obj.canSplit(arr);
+        cout << (res ? "true" : "false") << endl;
+    }
+    return 0;
+}
+// } Driver Code Ends
