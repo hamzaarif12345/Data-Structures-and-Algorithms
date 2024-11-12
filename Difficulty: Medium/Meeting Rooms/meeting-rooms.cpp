f1417a59dcd5670e12484851462774add371cd7b
Hamza Arif <hamza81611@gmail.com>
2024-11-12 21:18:20 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Meeting Rooms/meeting-rooms.cpp
@@ -0,0 +1,49 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+// User function Template for C++
+class Solution {
+public:
+    bool canAttend(vector<vector<int>> &arr) {
+        // Sort intervals by start time (arr[i][0]); if start times are the same, sort by end time (arr[i][1])
+        sort(arr.begin(), arr.end(), [](const vector<int> &a, const vector<int> &b) {
+            if (a[0] != b[0]) 
+                return a[0] < b[0];  // Sort by start time in ascending order
+            return a[1] < b[1];      // Sort by end time if start times are equal
+        });
+
+        for (int i = 1; i < arr.size(); i++) {
+            if (arr[i - 1][1] > arr[i][0]) return false;
+        }
+        return true;
+    }
+};
+
+//{ Driver Code Starts.
+int main() {
+    int tc;
+    cin >> tc;
+    while (tc--) {
+        int n;
+        cin >> n;
+        vector<vector<int>> arr(n);
+        for (int i = 0; i < n; i++) {
+            int x, y;
+            cin >> x >> y;
+            arr[i].push_back(x);
+            arr[i].push_back(y);
+        }
+        cin.ignore();
+        Solution obj;
+        bool ans = obj.canAttend(arr);
+        if (ans)
+            cout << "true" << endl;
+        else
+            cout << "false" << endl;
+    }
+    return 0;
+}
+// } Driver Code Ends
